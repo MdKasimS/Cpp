3fd81850550b093fe09e14ebd5a13890a1a0cfc2
smk_properties <45384577+MdKasimS@users.noreply.github.com>
2021-11-14 23:33:18 +0530

    Add files via upload

---
+++ cpp_stdlib.cpp
@@ -0,0 +1,211 @@
+/*
+=====================================================
+                C++ Standard Library
+        (Including Implementation By MS)
+=====================================================
+
+1. Algorithms
+    
+    <algorithm>, <cstdlib>, <numeric>
+
+2. Atomic Operations
+
+    <atomic>
+
+3. C library wrappers
+
+        <cassert>, <ccomplex>, <cctype>, <cerrno>, 
+        <cfenv>, <cfloat>, <cinttypes>, <ciso646>, <climits>,
+        <clocale>, <cmath>, <csetjmp>, <csignal>, <cstdalign>,
+        <cstdarg>, <cstdbool>, <cstddef>, <cstdint>11, <cstdio>,
+        <cstdlib>, <cstring>, <ctgmath>, <ctime>, <cuchar>, 
+        <cwchar>, <cwctype>
+
+4. Concepts
+
+        <concepts>
+
+5. Sequence containers
+
+        <array>, <deque>, <forward_list>, <list>, <vector>
+
+6. Ordered Associative Containers
+
+        <map>, <set>
+
+7. Unordered Associative Containers
+
+        <unorderd-map>, <unordered_set>
+    
+8. Container Adopters
+
+        <queue>, <stack>
+
+9. Container Views
+
+        <span>
+
+10. Errors And Exception Handling
+
+    <cassert>, <exception>, <stdexcept>, <system_error>
+
+11. General Utilities
+
+    <any>, <bit>, <bitset>, <cstdlib>, <execution>, 
+    <functional>, <memory>, <memory_resource>, <optional>, 
+    <ratio>, <scoped_allocator>, <tuple>, <type_traits>, 
+    <typeindex>, <utility>, <variant>    
+
+12. I/O And Formatting
+
+    <cinttypes>, <cstdio>, <filesystem>, <fstream>, 
+    <iomanip>, <ios>, <iosfwd>, <iostream>, <istream>, 
+    <ostream>, <sstream>, <streambuf>, <strstream> 
+
+13. iterators
+
+    <iterator>
+
+14. Language Support
+
+    <cfloat>, <climits>, <codecvt>, <compare>, 
+    <contract>, <coroutine>, <csetjmp>, <csignal>, 
+    <cstdarg>, <cstddef>, <cstdint>, <cstdlib>, 
+    <exception>, <initializer_list>, <limits>, <new>, 
+    <typeinfo>, <version>
+
+15. Lopcalisation
+
+    <clocale>, <codecvt>, <cvt/wbuffer>, <cvt/wstring>, <locale>
+
+16. Math And Numerics
+
+    <bit>, <cfenv>, <cmath>, <complex>, 
+    <cstdlib>, <limits>, <numeric>, <random>, 
+    <ratio>, <valarray>
+
+17. Memory Management
+
+    <allocators>, <memory>, <memory_resource>, <new>,
+    <scoped-allocator>
+
+18. Multithreading
+
+    <atomic>, <condition_variable>, <future>, <mutex>,
+    <shared_mutex>, <thread>
+
+19. Ranges
+    
+    <ranges>
+
+20. Strings and Character Data
+
+    <charconv>, <cctype>, <cstdlib>, <cstring>, <cuchar>,
+    <cwchar>, <cwtype>, <regex>, <string>, <string_view>
+
+21. Time
+    
+    <chrono>, <ctime>
+
+*/
+
+
+
+
+
+#include <iostream>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <tuple>
+#include <utility>
+#include <locale>
+#include <codecvt>
+#include <string>
+#include <regex>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <ostream>
+#include <sstream>
+#include <streambuf>
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <complex>
+#include <random>
+#include <valarray>
+#include <numeric>
+#include <cstdlib>
+#include <atomic>
+#include <cassert>
+#include <ccomplex>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <cinttypes>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdalign>
+#include <cstdarg>
+#include <cstdbool>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctgmath>
+#include <ctime>
+#include <cuchar>
+#include <cwchar>
+#include <cwctype>
+//#include <concepts> cpp20 edition
+#include <system_error>
+#include <any>
+#include <bit>
+#include <cstdlib>
+#include <random>
+//#include <allocators>
+#include <scoped_allocator>
+#include <memory>
+#include <shared_mutex>
+#include <cwchar>
+#include <cwctype>
+#include <string>
+#include <charconv>
+#include <string_view>
+
+
+using namespace std;
+
+int main(int argc, char *args[])
+{
+	cout<<" Assalamualaikum !!!"<<endl;
+	return 0;
+}
